02727824f7aa15ddbdf8d6261b1f92df68b8a251
Rick Teachey <ricky@teachey.org>
2019-12-09 10:22:15 -0500

    shorten code using DF vs DataFrame

--- msh2cande/msh_load.py
+++ msh2cande/msh_load.py
@@ -2,26 +2,28 @@ from pathlib import Path
 from typing import Sequence, NamedTuple
 import pandas as pd
 
+DF = pd.DataFrame
+
 
 class Msh(NamedTuple):
-    nodes: pd.DataFrame = None
-    elements: pd.DataFrame = None
-    boundaries: pd.DataFrame = None
+    nodes: DF = None
+    elements: DF = None
+    boundaries: DF = None
 
     @classmethod
     def load_msh(cls, path: Path):
         return _load_msh(path)
 
 
-def _n_nodes(msh_lines_df: pd.DataFrame) -> int:
+def _n_nodes(msh_lines_df: DF) -> int:
     n = msh_lines_df.iloc[0,0]
     msh_lines_df.drop(index=0, inplace=True)
     msh_lines_df.reset_index(inplace=True, drop=True)
     return int(n)
 
 
-def _nodes(msh_lines_df: pd.DataFrame, n_nodes: int) -> pd.DataFrame:
-    nodes_df: pd.DataFrame = msh_lines_df.iloc[:n_nodes, 0].str.strip().str.split(expand=True)
+def _nodes(msh_lines_df: DF, n_nodes: int) -> DF:
+    nodes_df: DF = msh_lines_df.iloc[:n_nodes, 0].str.strip().str.split(expand=True)
     nodes_df.columns = list("nxy")
     nodes_df["n"] = nodes_df["n"].apply(int)
     nodes_df.set_index("n", inplace=True)
@@ -31,15 +33,15 @@ def _nodes(msh_lines_df: pd.DataFrame, n_nodes: int) -> pd.DataFrame:
     return nodes_df
 
 
-def _n_elements(msh_lines_df: pd.DataFrame) -> int:
+def _n_elements(msh_lines_df: DF) -> int:
     n = msh_lines_df.iloc[0,0]
     msh_lines_df.drop(index=0, inplace=True)
     msh_lines_df.reset_index(inplace=True, drop=True)
     return int(n)
 
 
-def _elements(msh_lines_df: pd.DataFrame, n_elements: int) -> pd.DataFrame:
-    elements_df: pd.DataFrame = msh_lines_df.iloc[:n_elements, 0].str.strip().str.split(expand=True)
+def _elements(msh_lines_df: DF, n_elements: int) -> DF:
+    elements_df: DF = msh_lines_df.iloc[:n_elements, 0].str.strip().str.split(expand=True)
     elements_df.columns = list("eijkl")
     elements_df = elements_df.applymap(int)
     elements_df.set_index("e", inplace=True)
@@ -48,7 +50,7 @@ def _elements(msh_lines_df: pd.DataFrame, n_elements: int) -> pd.DataFrame:
     return elements_df
 
 
-def _boundaries(msh_lines: pd.DataFrame) -> pd.DataFrame:
+def _boundaries(msh_lines: DF) -> DF:
     boundaries_df = msh_lines.iloc[:, 0].str.strip().str.split(expand=True)
     boundaries_df.columns = list("bn")
     boundaries_df = boundaries_df[~boundaries_df["n"].isnull()]
@@ -60,17 +62,17 @@ def _boundaries(msh_lines: pd.DataFrame) -> pd.DataFrame:
     return boundaries_df
 
 
-def _load_msh_df(msh_str: Sequence[str]) -> pd.DataFrame:
-    return pd.DataFrame(msh_str, columns=["lines"])
+def _load_msh_df(msh_str: Sequence[str]) -> DF:
+    return DF(msh_str, columns=["lines"])
 
 
 def _load_msh_seq(msh_str: Sequence[str]) -> Msh:
     msh_lines_df = _load_msh_df(msh_str)
     n_nodes: int = _n_nodes(msh_lines_df)
-    nodes: pd.DataFrame = _nodes(msh_lines_df, n_nodes)
+    nodes: DF = _nodes(msh_lines_df, n_nodes)
     n_elements: int = _n_elements(msh_lines_df)
-    elements: pd.DataFrame = _elements(msh_lines_df, n_elements)
-    boundaries: pd.DataFrame = _boundaries(msh_lines_df)
+    elements: DF = _elements(msh_lines_df, n_elements)
+    boundaries: DF = _boundaries(msh_lines_df)
     return Msh(nodes, elements, boundaries)
 
 
