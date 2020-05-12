801e238b3ec635e58c278ae2edad8195b5707e4a
ricky <ricky@teachey.org>
2020-05-12 14:44:33 -0400

    Added sequence view/delegation objects

--- msh2cande/cande.py
+++ msh2cande/cande.py
@@ -1,3 +1,5 @@
+import numbers
+from abc import abstractmethod
 from enum import Enum
 from functools import partial
 import pandas as pd
@@ -20,3 +22,163 @@ class L3(Enum):
 def _L3_format(df: pd.DataFrame, l3_fmt: L3):
     df_reset = df.reset_index() if l3_fmt is not L3.c5 else df
     return df_reset.apply(lambda srs: l3_fmt(**srs), axis=1)
+
+
+class SeqItemView:
+    def __init__(self, key, seq):
+        self.key = key
+        self.seq = seq
+
+    @property
+    def value(self):
+        return self.seq[self.key]
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}({self.seq!r}, {self.key!r})"
+
+    def __str__(self):
+        return f"{self.seq[self.key]!s}"
+
+    def __bool__(self):
+        return bool(self.value)
+
+    def __eq__(self, other):
+        return self.value == other
+
+
+import math
+
+
+class DelegatedInteger(numbers.Integral):
+    @property
+    @abstractmethod
+    def value(self):
+        return NotImplemented
+
+    def __abs__(self):
+        return abs(self.value)
+
+    def __add__(self, other):
+        return self.value + other
+
+    def __ceil__(self):
+        return math.ceil(self.value)
+
+    def __eq__(self, other):
+        return self.value == other
+
+    def __floor__(self):
+        return math.floor(self.value)
+
+    def __floordiv__(self, other):
+        return self.value // other
+
+    def __int__(self):
+        return int(self.value)
+
+    def __le__(self, other):
+        return self.value <= other
+
+    def __lt__(self, other):
+        return self.value < other
+
+    def __mod__(self, other):
+        return self.value % other
+
+    def __mul__(self, other):
+        return self.value * other
+
+    def __neg__(self):
+        return -self.value
+
+    def __pos__(self):
+        return +self.value
+
+    def __pow__(self, other):
+        return self.value ** other
+
+    def __radd__(self, other):
+        return other + self.value
+
+    def __rfloordiv__(self, other):
+        return other // self.value
+
+    def __rmod__(self, other):
+        return other % self.value
+
+    def __rmul__(self, other):
+        return other * self.value
+
+    def __round__(self, ndigits=None):
+        return round(self.value, ndigits)
+
+    def __rpow__(self, other):
+        return other ** self.value
+
+    def __rtruediv__(self, other):
+        return other / self.value
+
+    def __truediv__(self, other):
+        return self.value / other
+
+    def __trunc__(self):
+        return math.trunc(self.value)
+
+    def __and__(self, other):
+        return self.value & other
+
+    def __lshift__(self, other):
+        return self.value << other
+
+    def __invert__(self):
+        return ~self.value
+
+    def __or__(self, other):
+        return self.value | other
+
+    def __rand__(self, other):
+        return other & self.value
+
+    def __rlshift__(self, other):
+        return other << self.value
+
+    def __ror__(self, other):
+        return other | self.value
+
+    def __rrshift__(self, other):
+        return other >> self.value
+
+    def __rshift__(self, other):
+        return self.value >> other
+
+    def __rxor__(self, other):
+        return other ^ self.value
+
+    def __xor__(self, other):
+        return self.value ^ other
+
+
+class SeqNumberView(SeqItemView, DelegatedInteger):
+    def __iadd__(self, other):
+        self.seq[self.key] += other
+
+    def __isub__(self, other):
+        self.seq[self.key] -= other
+
+    def __imul__(self, other):
+        self.seq[self.key] *= other
+
+    def __itruediv__(self, other):
+        self.seq[self.key] /= other
+
+    def __ifloordiv__(self, other):
+        self.seq[self.key] //= other
+
+    def __imod__(self, other):
+        self.seq[self.key] %= other
+
+    def __imatmul__(self, other):
+        self.seq[self.key] @= other
+
+    def __ipow__(self, other):
+        self.seq[self.key] **= other
---
+++ tests/test_cande.py
@@ -0,0 +1,80 @@
+import pytest
+
+from msh2cande.cande import DelegatedInteger, SeqItemView, SeqNumberView
+
+
+@pytest.fixture
+def delegated_integer_class():
+    class C(DelegatedInteger):
+        v = 1
+        @property
+        def value(self):
+            return self.v
+    return C
+
+
+@pytest.fixture
+def delegated_obj(delegated_integer_class):
+    return delegated_integer_class()
+
+
+def test_delegated_integer_obj(delegated_obj):
+    assert delegated_obj
+    assert delegated_obj + 1 == 2
+    assert delegated_obj - 1 == 0
+    assert delegated_obj * 5 == 5
+    assert delegated_obj / 2 == 0.5
+    assert delegated_obj // 2 == 0
+    assert delegated_obj ** 0.5 == 1.0
+    assert -delegated_obj == -1
+    assert +-delegated_obj == -1
+    assert abs(+-delegated_obj) == 1
+    assert delegated_obj % 2 == 1
+    assert delegated_obj % 1 == 0
+
+
+@pytest.fixture
+def seq():
+    return [1,2,3,4,5,6]
+
+
+@pytest.fixture
+def key():
+    return 3
+
+
+@pytest.fixture
+def seq_item_view_obj(seq, key):
+    return SeqItemView(key, seq)
+
+
+def test_seq_item_view_obj(seq_item_view_obj, seq, key):
+    assert seq_item_view_obj.value == seq[key]
+    assert seq_item_view_obj == seq[key]
+    assert repr(seq_item_view_obj) == f"SeqItemView({seq!r}, {key!r})"
+    assert str(seq_item_view_obj) == f"{seq[key]!s}"
+    assert not bool(seq_item_view_obj) == False
+
+
+@pytest.fixture
+def seq_number_view_obj(seq, key):
+    return SeqNumberView(key, seq)
+
+
+import operator as op
+
+
+@pytest.mark.parametrize("ifunc, func",
+                         [
+                             (op.iadd, op.add),
+                             (op.isub, op.sub),
+                             (op.imul, op.mul),
+                             (op.itruediv, op.truediv),
+                             (op.ifloordiv, op.floordiv),
+                             (op.imod, op.mod),
+                             (op.ipow, op.pow),
+                         ])
+def test_seq_number_view_obj_operators(ifunc, func, seq_number_view_obj, seq, key):
+    original = seq[key]
+    ifunc(seq_number_view_obj, 1)
+    assert seq[key] == func(original, 1)
